b05a49b0a722477eee62140a33c1e687eb2cecb9
DallasRose88 <158493720+DallasRose88@users.noreply.github.com>
2024-08-05 21:52:05 +0200

    Update Day 3.py

--- Day 3.py
+++ Day 3.py
@@ -17,3 +17,24 @@ for line in f:
         total += (num - 96)
 print('Part 1', total)
 #Part 2
+f = open('code', 'r')
+
+content = f.readlines()
+
+total = 0
+x = 0
+while x < len(content):
+    a= content[x].strip()
+    b = content[x+1]
+    c = content[x+2]
+    letter = ''
+    for y in a:
+        if y in b and y in c:
+            letter = y
+    num = ord(letter)
+    if num < 91:
+        total += (num - 38)
+    if num > 90:
+        total += (num - 96)
+    x += 3
+print("Part 2", total)
