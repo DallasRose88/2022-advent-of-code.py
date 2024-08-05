5692c9d6c0041bf1a758b76d9b4bc57016234046
DallasRose88 <158493720+DallasRose88@users.noreply.github.com>
2024-08-05 12:55:35 +0200

    Create Day 3.py

---
+++ Day 3.py
@@ -0,0 +1,23 @@
+f = open('Trial2.txt','r')
+
+total = 0
+letters = []
+for line in f:
+    letter = ''
+    a = (len(line.strip()))
+    div = a//2
+    first = line[0:div]
+    second = line[div:]
+    for x in first:
+        if x in second:
+            letter = x 
+    letter.
+    num = ord(letter)
+    if num > 90:
+        num -=96
+        total += num
+    else:
+        num -= 38
+        total += num
+print('part 1',total)
+#Part 2
