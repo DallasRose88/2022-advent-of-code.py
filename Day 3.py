3e7ec24628364cb374e6257885738f7dd904aa0e
DallasRose88 <158493720+DallasRose88@users.noreply.github.com>
2024-08-05 16:50:29 +0200

    Update Day 3.py

--- Day 3.py
+++ Day 3.py
@@ -1,21 +1,34 @@
-f = open('Trial2.txt','r')
+f = open('trial.txt','r')
 
 total = 0
+total2 = 0
 for line in f:
-    letter = ''
-    a = (len(line.strip()))
-    div = a//2
-    first = line[0:div]
-    second = line[div:]
-    for x in first:
-        if x in second:
-            letter = x 
-    num = ord(letter)
-    if num > 90:
-        num -=96
-        total += num
-    else:
-        num -= 38
-        total += num
-print('part 1',total)
-#Part 2
+    replace = line.replace(',','-').strip()
+    split = replace.split('-')
+    x = int(split[0])
+    rooms = []
+    while x < int(split[1])+1:
+        rooms.append(x)
+        x += 1 
+    y = int(split[2])
+    room2 = []
+    while y < int(split[3]) +1:
+        room2.append(y)
+        y += 1 
+    count = 0 
+    count3 = 0
+    for x in rooms:
+        if x not in room2:
+            count += 1 
+        else:
+            count3 += 1
+    count2 = 0
+    for y in room2:
+        if y not in rooms:
+            count2 += 1
+    if count == 0 or count2 == 0:
+        total += 1
+    if count3 > 0:
+        total2 += 1
+print('Part 1', total)
+print('Part 2', total2)
